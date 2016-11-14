8fb2baa9cd4e25e90feed20389de85e0acdbf2e9
gdanezis <g.danezis@ucl.ac.uk>
2016-11-14 21:12:33 +0000

    Added init

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,6 @@ print(os.listdir(prev))
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('.'))
 
-import os
 print("Current Directory: %s" % os.getcwd())
 print("Current Path: %s" % str(sys.path))
 
