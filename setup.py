0f5be1fba222a9522c86c55e39de8f07cd8ea02d
gdanezis <g.danezis@ucl.ac.uk>
2016-12-21 18:33:29 +0000

    Managed to compile the cypthon part

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+from setuptools.extension import Extension
+from Cython.Build import cythonize
 
 import sphinxmix
+
+
 
 if __name__ == "__main__":
       
       setup(name='sphinxmix',
+            # packages = find_packages(),
+            # ext_modules = cythonize("sphinxmix/SphinxCrypto.pyx", libraries = ['crypto']),
+            ext_modules = cythonize([
+                Extension("sphinxmix.SphinxCrypto", ["sphinxmix/SphinxCrypto.pyx"],
+                          libraries=["crypto"])
+                ]),
             version=sphinxmix.VERSION,
             description='A Python implementation of the Sphinx mix packet format.',
             author='George Danezis',
@@ -17,8 +27,13 @@ if __name__ == "__main__":
             For full documentation see: http://sphinxmix.readthedocs.io/en/latest/
             """,
 
-            setup_requires=['pytest-runner', "pytest"],
+            package_data = {
+                  'sphinxmix': ['*.pyx'],
+            },
+
+            setup_requires=['pytest-runner', "pytest", "Cython"],
             tests_require=[
+                  "Cython",
                   "pytest",
                   "future >= 0.14.3",
                   "pytest >= 3.0.0",
@@ -26,6 +41,7 @@ if __name__ == "__main__":
                   "petlib >= 0.0.38",
             ],
             install_requires=[
+                  "Cython",
                   "future >= 0.14.3",
                   "pytest >= 3.0.0",
                   "msgpack-python >= 0.4.6",
